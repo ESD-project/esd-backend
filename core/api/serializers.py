ffd40c11248c7a15c1ee14dbceabaed5c5621203
DevPrinceK <princesamuelpks@gmail.com>
2023-03-24 22:40:23 +0000

    Create serializers.py

---
+++ core/api/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import authenticate
+from entities.models import User
+from rest_framework import serializers
+from rest_framework.response import Response
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return Response(user, status=200)
+        raise Response(
+            {"error": "Unable to log in with provided credentials."}, status=400)
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('fullname', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            fullname=validated_data['fullname'],
+        )
+        return user
