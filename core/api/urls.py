326c86764cf188673ecb7a98bbd5144fbe4d8424
DevPrinceK <princesamuelpks@gmail.com>
2023-03-24 23:21:21 +0000

    Update urls.py

--- core/api/urls.py
+++ core/api/urls.py
@@ -6,8 +6,29 @@ app_name = "api"
 urlpatterns = [
     path('', views.OverViewEndpoint.as_view(), name='overview'),
     path('register/', views.RegisterStaffAPI.as_view(), name="register"),
+    path('register-user/', views.RegisterUserAPI.as_view(), name="register_user"),
 ]
 
+
+# cars
+urlpatterns += [
+    path('cars/', views.CarListAPI.as_view(), name='car_list'),
+    path('cars-detail/', views.CarDetailAPI.as_view(), name='car_detail'),
+    path('create-update-car/', views.CreateUpdateCarAPI.as_view(), name='create_update_car'),  # noqa
+    path('delete-car/', views.DeleteCarAPI.as_view(), name='car_update'),
+]
+
+# rentals
+urlpatterns += [
+    path('rentals/', views.RentalListAPI.as_view(), name='rental_list'),
+    path('rentals-detail/', views.RentalDetailAPI.as_view(), name='rental_detail'),
+]
+
+# rental slots
+urlpatterns += [
+    path('rental-slots/', views.RentalSlotListAPI.as_view(), name='rental_slot_list'),  # noqa
+    path('rental-slots-detail/', views.RentalSlotDetailAPI.as_view(), name='rental_slot_detail'),  # noqa
+]
 
 # API AUTHENTICATION
 urlpatterns += [
